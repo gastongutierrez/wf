3c6e9f33bc5dbf8f463b81053290b1a95407b50a
Gaston Gutierrez <gastongutierrez@gmail.com>
2025-07-29 00:29:54 -0400

    feat: add myapp.py

---
+++ myapp.py
@@ -0,0 +1,27 @@
+import logging
+from argparse import ArgumentParser
+from task_scheduler.scheduler import Scheduler
+import sys
+import tasks
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-t", "--tasks-file", default="tasks.txt")
+    parser.add_argument("-m", "--tasks-module", default="tasks.py")
+    parser.add_argument("-v", "--validate-tasks", action="store_true")
+    parser.add_argument("-r", "--run-tasks", action="store_true")
+    args = parser.parse_args()
+
+    try:
+        scheduler = Scheduler("MyAppScheduler", args.tasks_file)
+        if args.validate_tasks:
+            scheduler.validate_tasks()
+        if args.run_tasks:
+            scheduler.run_tasks()
+    except ValueError as e:
+        logging.error(str(e))
+        sys.exit(1)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
+    main()
